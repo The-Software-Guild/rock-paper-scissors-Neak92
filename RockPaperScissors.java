63412104998ddc2a79bac914af8e81a881058f3b
Neak92 <47485245+Neak92@users.noreply.github.com>
2022-02-17 17:01:40 +0000

    Add files via upload

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -6,24 +6,30 @@ import java.util.Scanner;
 public class RockPaperScissors {
     public static void main(String[] args) {
 
+        // Ask user for number of round he would like to play
+
         System.out.print("How many rounds would you like to play ? ");
         Scanner inputReader = new Scanner(System.in);
 
         int nbRounds = inputReader.nextInt();
-        if (nbRounds<11 && nbRounds>0){  play(nbRounds);
+
+        //Checking for number of round to be between 1 and 10
+        if (nbRounds<11 && nbRounds>0){
+            int result = play(nbRounds);
+
             System.out.println("");
             System.out.println("");
-            System.out.println("Thanks for playing!");
+          if (result == 0) System.out.println("Thanks for playing!");
         }
         else System.out.println("Error, invalid input");
+}
 
-
-    }
-
-    private static void play(int nbRounds) {
+    private static int play(int nbRounds) {
         int wins = 0;
         int ties = 0;
         int lost = 0;
+
+        // Instructions for the player to play
 
         System.out.println("To play select 1 for Rock, 2 for Paper or 3 for Scissors");
         System.out.println("Rock win over Scissors, Paper wins over Rock and Scissors win over Paper");
@@ -31,16 +37,29 @@ public class RockPaperScissors {
 
         Random randomizer = new Random();
 
-        int x =  0;
-        while (x < nbRounds){
+        int round =  0;
+
+        //While loop representing each round played until desired number is reached
+
+        while (round < nbRounds){
 
             System.out.println("");
             System.out.println("What would you like to play? ");
             int playerPlay = inputReader.nextInt();
 
+            // Checking for incorrect input
+
+            while (playerPlay < 1 || playerPlay > 3 ){
+                System.out.println("Incorrect input try again  ");
+                System.out.println("What would you like to play? ");
+                 playerPlay = inputReader.nextInt();
+
+            }
             int computerPlay = randomizer.nextInt(3)+1;
-          //  System.out.println(computerPlay);
+
             System.out.println("");
+
+            // Decision of the round's result
 
             if (computerPlay == playerPlay){ ties++; System.out.println("You tied this round");}
 
@@ -48,36 +67,47 @@ public class RockPaperScissors {
             else if (playerPlay == 2 && computerPlay == 1){ wins++; System.out.println("You won this round");}
             else if (playerPlay == 3 && computerPlay == 2){ wins++; System.out.println("You won this round");}
 
-            else if (playerPlay == 3 && computerPlay == 1){ lost++; System.out.println("You lost this round");}
-            else if (playerPlay == 2 && computerPlay == 3){ lost++; System.out.println("You lost this round");}
-            else if (playerPlay == 1 && computerPlay == 2){ lost++; System.out.println("You lost this round");}
+            else { lost++; System.out.println("You lost this round");}
 
 
+            // Decision when the number of round is reached
 
-            if((x+1) == nbRounds) {
+            if((round+1) == nbRounds) {
 
                 System.out.println("");
                 System.out.println("Game Over");
-                int r =0 ;
-               r =  result(wins,ties,lost);
-                if (r == 1 ){
+                int decision = 0 ;
 
-                     wins = 0;
-                     ties = 0;
-                     lost = 0;
-                     x = 0;
-                    System.out.println("Let's play again");
+                // Showing player result and asking for a new game
+                decision =  result(wins,ties,lost);
+                if (decision == 1 ){
+
+                    System.out.print("How many rounds would you like to play ? ");
+
+
+                     nbRounds = inputReader.nextInt();
+
+                    if (nbRounds<11 && nbRounds>0){
+                        wins = 0;
+                        ties = 0;
+                        lost = 0;
+                        round = 0;
+                        System.out.println("Let's play again");
+                    }
+                    else { System.out.println("Error, invalid input"); return 1;}
+
+
                 }
-                else x++;
+                else round++;
             }
-            else x++;
+            else round++;
         }
-    }
+    return 0;}
 
     private static int result(int wins, int ties, int lost) {
 
         System.out.println("");
-        if (wins > lost) System.out.print("Congrulations, you won       ");
+        if (wins > lost) System.out.print("Congratulations, you won       ");
         else if (wins < lost) System.out.println("Sorry, you lost maybe next time       ");
         else System.out.println("You're tied, try again to win next time        ");
 
